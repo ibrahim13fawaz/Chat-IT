3236fb9768295e0b3c5fba3dc5a74a65db8df951
ibrahim13fawaz <hakeerg333@gmail.com>
2026-04-26 23:33:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,12 @@
 // ============================================
 
 export const firebaseConfig = {
-apiKey: “YOUR_API_KEY”,
-authDomain: “YOUR_PROJECT_ID.firebaseapp.com”,
-databaseURL: “https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com”,
-projectId: “YOUR_PROJECT_ID”,
-storageBucket: “YOUR_PROJECT_ID.appspot.com”,
-messagingSenderId: “YOUR_SENDER_ID”,
-appId: “YOUR_APP_ID”
+  apiKey: "AIzaSyCXA1x9fJe6zPFo7yiK1kSRsoR89aSff5k",
+  authDomain: "itchat-web-8c4ed.firebaseapp.com",
+  databaseURL: "https://itchat-web-8c4ed-default-rtdb.firebaseio.com",
+  projectId: "itchat-web-8c4ed",
+  storageBucket: "itchat-web-8c4ed.firebasestorage.app",
+  messagingSenderId: "787261764804",
+  appId: "1:787261764804:web:7af9a924d03989b1dbc591",
+  measurementId: "G-1KPE5B71QR"
 };
